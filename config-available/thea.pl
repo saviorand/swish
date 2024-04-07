a9a29665904a71027321c550c3d736a865e47311
Valentin Erokhin <37780080+saviorand@users.noreply.github.com>
2024-04-07 15:49:32 +0000

    test

--- config-available/thea.pl
+++ config-available/thea.pl
@@ -1,7 +1,3 @@
-:- use_module('../thea/owl2_model').
-:- use_module('../thea/owl2_from_rdf').
-:- use_module('../thea/owl2_export_rdf').
-:- use_module('../thea/owl2_xml').
-:- use_module('../thea/owl2_util').
 :- use_module('../thea/owl2_io').
+:- use_module('../thea/owl2_from_rdf').
 :- use_module('../thea/owl2_to_prolog_dlp').
